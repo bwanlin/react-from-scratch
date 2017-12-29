4a432b6aeebfa76f36872445c48316ea997024e3
Brian <wanlin.brian@gmail.com>
2017-12-29 21:42:19 +0100

    settings up react, webpack css, images, babel js

---
+++ src/index.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+import '../assets/style/index.css'
+
+class App extends React.Component {
+    render() {
+        return (
+					<div className="text-red">homepage</div>
+        )
+    }
+}
+
+ReactDOM.render(
+  <App/>,
+	app
+)
+
+//module.hot.accept();
